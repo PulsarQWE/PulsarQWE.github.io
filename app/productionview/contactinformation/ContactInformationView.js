46cfa0f2a2cad0c867dc77f2fa95983f1ff2177d
PulsarQWE <Alex.3245@mail.ru>
2018-05-12 22:12:14 +0300

    Рефакторинг контактной информации

--- app/productionview/contactinformation/ContactInformationView.js
+++ app/productionview/contactinformation/ContactInformationView.js
@@ -20,91 +20,81 @@ Ext.define('DiplomSite.productionview.contactinformation.ContactInformationView'
             },
             items: [
                 {
-                    xtype: 'label',
-                    html: 'Адреса электронной почты',
-                    margin: '10 10 10 10'
-                },
-                {
-                    xtype: 'button',
-                    text: '89301751763@mail.ru',
-                    handler:function(){
-                        document.location.href = 'mailto:89301751763@mail.ru'
+                    xtype: 'container',
+                    layout: {
+                        type: 'vbox',
+                        align: 'stretch'
                     },
-                    margin: '5 10 10 0'
+                    items:[
+                        {
+                            xtype: 'label',
+                            html: 'Адреса электронной почты',
+                            margin: '8 15 60 10'
+                        },
+                        {
+                            xtype: 'label',
+                            html: 'Связаться в контакте',
+                            margin: '0 15 0 10'
+                        }
+                    ]
                 },
                 {
-                    xtype: 'label',
-                    html: ' - Алексей',
-                    margin: '10 10 10 0'
-                }
-            ]
-        },
-        {
-            xtype: 'container',
-            layout: {
-                type: 'hbox',
-                align: 'stretch'
-            },
-            items: [
-                {
-                    xtype: 'label',
-                    html: '',
-                    margin: '10 10 10 160'
-                },
-                {
-                    xtype: 'button',
-                    text: 'sobolev251997@mail.ru',
-                    handler:function(){
-                        document.location.href = 'mailto:sobolev251997@mail.ru'
+                    xtype: 'container',
+                    layout: {
+                        type: 'vbox',
+                        align: 'stretch'
                     },
-                    margin: '5 10 10 0'
+                    items: [
+                        {
+                            xtype: 'button',
+                            text: 'Алексей',
+                            handler:function(){
+                                document.location.href = 'mailto:89301751763@mail.ru'
+                            },
+                            margin: '5 10 10 0'
+                        },
+                        {
+                            xtype: 'button',
+                            text: 'Илья',
+                            handler:function(){
+                                document.location.href = 'mailto:sobolev251997@mail.ru'
+                            },
+                            margin: '5 10 10 0'
+                        },
+                        {
+                            xtype: 'button',
+                            text: 'Алексей',
+                            href: 'https://vk.com/pulsar_qwe',
+                            target: '_blank',
+                            margin: '5 10 10 0'
+                        },
+                        {
+                            xtype: 'button',
+                            text: 'Илья',
+                            href: 'https://vk.com/iljas25',
+                            target: '_blank',
+                            margin: '5 10 10 0'
+                        }
+                    ]
                 },
                 {
-                    xtype: 'label',
-                    html: ' - Илья',
-                    margin: '10 10 10 0'
-                }
-            ]
-        },
-        {
-            xtype: 'container',
-            layout: {
-                type: 'hbox',
-                align: 'stretch'
-            },
-            items: [
-                {
-                    xtype: 'label',
-                    html: 'Связаться в контакте',
-                    margin: '10 10 10 10'
-                },
-                {
-                    xtype: 'button',
-                    text: 'Алексей',
-                    href: 'https://vk.com/pulsar_qwe',
-                    target: '_blank',
-                    margin: '5 10 10 32'
-                }
-            ]
-        },
-        {
-            xtype: 'container',
-            layout: {
-                type: 'hbox',
-                align: 'stretch'
-            },
-            items: [
-                {
-                    xtype: 'label',
-                    html: '',
-                    margin: '10 10 10 160'
-                },
-                {
-                    xtype: 'button',
-                    text: 'Илья',
-                    href: 'https://vk.com/iljas25',
-                    target: '_blank',
-                    margin: '5 10 10 0'
+                    xtype: 'container',
+                    layout: {
+                        type: 'vbox',
+                        align: 'stretch'
+                    },
+                    items: [
+                        {
+                            xtype: 'label',
+                            html: '- 89301751763@mail.ru',
+                            margin: '8 15 23 10'
+                        },
+                        {
+                            xtype: 'label',
+                            html: '- sobolev251997@mail.ru',
+                            margin: '0 15 0 10'
+                        }
+                    ]
                 }
             ]
         },
